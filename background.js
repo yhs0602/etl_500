8bf8888a15c784d02b03e583881493849673bc16
yhs0602 <jourhyang123@gmail.com>
2023-12-09 14:31:36 +0900

    :tada: Implement code

--- background.js
+++ background.js
@@ -1,12 +1,25 @@
-/**
-* ---------------------------------------------------------------------------------
-* | 백그라운드 |
-* ---------------------------------------------------------------------------------
-* - 디폴트 컬러를 지정하여 스토리지 API 를 호출하여 지정한 색을 저장시킵니다.
-**/
-let color = '#3aa757';
+chrome.webRequest.onCompleted.addListener(
+  (details) => {
+    // Check if the status code is 500
+    if (details.statusCode === 500) {
+      console.log("HTTP 500 detected. Clearing cookies and reloading.");
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('기본 배경색은 %cgreen', `color: ${color}`);
-});
+      // Replace 'https://myetl.snu.ac.kr/' with the actual host
+      let url = 'https://myetl.snu.ac.kr/';
+
+      // Clear cookies
+      chrome.cookies.getAll({ url: url }, function (cookies) {
+        for (let i = 0; i < cookies.length; i++) {
+          let cookie = cookies[i];
+          let url = (cookie.secure ? "https://" : "http://") + cookie.domain + cookie.path;
+          chrome.cookies.remove({ url: url, name: cookie.name });
+        }
+      });
+
+      // Reload the tab
+      chrome.tabs.reload(details.tabId);
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["responseHeaders"]
+);
